e8d7f82501f90d332582c49c5aaa9b9b1d8c8736
dimasm93 <software.dimas_m@icloud.com>
2019-08-28 09:33:27 +0700

    init project

---
+++ src/main/java/com/maryanto/dimas/springboot2trainingtelkom2019/Springboot2TrainingTelkom2019Application.java
@@ -0,0 +1,13 @@
+package com.maryanto.dimas.springboot2trainingtelkom2019;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2TrainingTelkom2019Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2TrainingTelkom2019Application.class, args);
+	}
+
+}
