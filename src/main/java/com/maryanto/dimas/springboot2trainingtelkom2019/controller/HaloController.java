ff108e06f8dc3b922bbc3bf590be6c6163bc94b2
dimasm93 <software.dimas_m@icloud.com>
2019-08-29 09:31:10 +0700

    update request parameter

--- src/main/java/com/maryanto/dimas/springboot2trainingtelkom2019/controller/HaloController.java
+++ src/main/java/com/maryanto/dimas/springboot2trainingtelkom2019/controller/HaloController.java
@@ -3,6 +3,7 @@ package com.maryanto.dimas.springboot2trainingtelkom2019.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Date;
 
@@ -10,9 +11,13 @@ import java.util.Date;
 public class HaloController {
 
     @GetMapping("/halo")
-    public String halo(Model model) {
+    public String halo(
+            Model model,
+            @RequestParam(required = false) String nama,
+            @RequestParam(required = false) Integer umur) {
         model.addAttribute("tanggal", new Date());
         model.addAttribute("message", "Halo ini dari bahasa indonesia");
+        System.out.println("nama dari request " + nama + ", umur dari request = " + umur);
         return "belajar-html";
     }
 }
