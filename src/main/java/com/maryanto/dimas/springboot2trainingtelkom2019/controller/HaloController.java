6cc6912517d94bc46aa7d4a0f54404b2c73c98cf
dimasm93 <software.dimas_m@icloud.com>
2019-08-29 10:16:46 +0700

    memuat request web

--- src/main/java/com/maryanto/dimas/springboot2trainingtelkom2019/controller/HaloController.java
+++ src/main/java/com/maryanto/dimas/springboot2trainingtelkom2019/controller/HaloController.java
@@ -1,8 +1,11 @@
 package com.maryanto.dimas.springboot2trainingtelkom2019.controller;
 
+import com.maryanto.dimas.springboot2trainingtelkom2019.model.Mahasiswa;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -11,11 +14,24 @@ public class HaloController {
     @GetMapping("/halo")
     public String halo(
             Model model,
+            @ModelAttribute Mahasiswa mahasiswa) {
+        model.addAttribute("mahasiswa", mahasiswa);
+        return "belajar-html";
+    }
+
+    @GetMapping("/haloPost")
+    public String requestGet(
             @RequestParam(required = false) String nama,
             @RequestParam(required = false) Integer umur) {
-        model.addAttribute("nama", nama);
-        model.addAttribute("umur", umur);
         System.out.println("nama dari request " + nama + ", umur dari request = " + umur);
-        return "belajar-html";
+        return "redirect:/halo";
+    }
+
+    @PostMapping("/haloPost")
+    public String requestPost(
+            @RequestParam(required = false) String nama,
+            @RequestParam(required = false) Integer umur) {
+        System.out.println("nama dari request " + nama + ", umur dari request = " + umur);
+        return "redirect:/halo";
     }
 }
---
+++ src/main/java/com/maryanto/dimas/springboot2trainingtelkom2019/model/Mahasiswa.java
@@ -0,0 +1,14 @@
+package com.maryanto.dimas.springboot2trainingtelkom2019.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Mahasiswa {
+
+    private String nama;
+    private Integer umur;
+}
