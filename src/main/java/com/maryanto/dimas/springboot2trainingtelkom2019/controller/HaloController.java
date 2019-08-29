22da9901311d74a60c37a9241dbb64453a4106c7
dimasm93 <software.dimas_m@icloud.com>
2019-08-29 08:57:18 +0700

    template engine dengan thymeleaf

--- src/main/java/com/maryanto/dimas/springboot2trainingtelkom2019/controller/HaloController.java
+++ src/main/java/com/maryanto/dimas/springboot2trainingtelkom2019/controller/HaloController.java
@@ -1,13 +1,18 @@
 package com.maryanto.dimas.springboot2trainingtelkom2019.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Date;
 
 @Controller
 public class HaloController {
 
     @GetMapping("/halo")
-    public String halo() {
+    public String halo(Model model) {
+        model.addAttribute("tanggal", new Date());
+        model.addAttribute("message", "Halo ini dari bahasa indonesia");
         return "belajar-html";
     }
 }
