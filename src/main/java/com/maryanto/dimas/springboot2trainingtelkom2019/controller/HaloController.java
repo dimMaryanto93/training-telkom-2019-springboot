d19fb2d2f0fbedc246de51d2906c5ca0840e5501
dimasm93 <software.dimas_m@icloud.com>
2019-08-30 10:06:00 +0700

    membuat fitur crud

--- src/main/java/com/maryanto/dimas/springboot2trainingtelkom2019/controller/HaloController.java
+++ src/main/java/com/maryanto/dimas/springboot2trainingtelkom2019/controller/HaloController.java
@@ -1,6 +1,8 @@
 package com.maryanto.dimas.springboot2trainingtelkom2019.controller;
 
+import com.maryanto.dimas.springboot2trainingtelkom2019.MahasiswaRepository;
 import com.maryanto.dimas.springboot2trainingtelkom2019.model.Mahasiswa;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,27 +10,19 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
 public class HaloController {
 
-    public HaloController() {
-        this.daftarMahasiswa.add(new Mahasiswa("Dimas Maryanto", 15));
-        this.daftarMahasiswa.add(new Mahasiswa("Mahasiswa 1", 15));
-        this.daftarMahasiswa.add(new Mahasiswa("Mahasiswa 2", 15));
-        this.daftarMahasiswa.add(new Mahasiswa("Mahasiswa 3", 15));
-    }
-
-    private List<Mahasiswa> daftarMahasiswa = new ArrayList<>();
+    @Autowired
+    private MahasiswaRepository repo;
 
     @GetMapping("/halo")
     public String halo(
             Model model,
             @ModelAttribute Mahasiswa mahasiswa) {
         model.addAttribute("mahasiswa", mahasiswa);
-        model.addAttribute("listMahasiswa", daftarMahasiswa);
+        Iterable<Mahasiswa> all = repo.findAll();
+        model.addAttribute("listMahasiswa", all);
         return "belajar-html";
     }
 
@@ -36,7 +30,7 @@ public class HaloController {
     public String requestPost(
             @RequestParam(required = false) String nama,
             @RequestParam(required = false) Integer umur) {
-        System.out.println("nama dari request " + nama + ", umur dari request = " + umur);
+        repo.save(new Mahasiswa(null, nama, umur));
 //        save to database
         return "redirect:/halo";
     }
--- src/main/java/com/maryanto/dimas/springboot2trainingtelkom2019/model/Mahasiswa.java
+++ src/main/java/com/maryanto/dimas/springboot2trainingtelkom2019/model/Mahasiswa.java
@@ -4,11 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "mahasiswa")
 public class Mahasiswa {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
     private String nama;
     private Integer umur;
 }
